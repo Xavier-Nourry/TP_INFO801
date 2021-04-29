b9ffce2ee8c7a7c91dc980487652fc572ca0a54e
Camille <43339411+Camille0907@users.noreply.github.com>
2021-04-29 14:32:15 +0200

    Amélioration de la classe LockTimer

--- TP_INFO801/src/main/java/com/INFO801/TP_INFO801/access_system/LockTimer.java
+++ TP_INFO801/src/main/java/com/INFO801/TP_INFO801/access_system/LockTimer.java
@@ -5,50 +5,88 @@ import org.jspace.FormalField;
 import org.jspace.RemoteSpace;
 
 public class LockTimer implements Runnable {
-    public static final Object ACTIVATION = "ActiveLockTimer";
-    private final String doorName;
-    private final String timerName;
+    public static final String ACTIVATION = "ActiveLockTimer";
+    private final String doorID;
+    private final String timerID;
+    private final RemoteSpace ts;
 
-    public LockTimer(String doorName) {
-        this.doorName = doorName;
-        this.timerName = doorName + " - LockTimer";
+    public LockTimer(String doorID) {
+        this.doorID = doorID;
+        this.timerID = doorID + " - LockTimer";
+
+        // Connexion à l'espace de tuple
+        ts = TupleSpace.remoteSpaceConnexion(timerID);
+    }
+
+    public LockTimer(LockTimer lockTimer) {
+        this.doorID = lockTimer.doorID;
+        this.timerID = lockTimer.timerID;
+        this.ts = lockTimer.ts;
     }
 
     @Override
     public void run() {
-        RemoteSpace ts = TupleSpace.remoteSpaceConnexion(timerName);
-        assert ts != null;
-
-        try {
-            ts.put(timerName, 0);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        this.monitorTimer(ts);
+        setInitialState();
+        monitor();
     }
 
-    private void monitorTimer(RemoteSpace ts) {
-        try {
-            Object[] activation = ts.get(new ActualField(doorName), new ActualField(ACTIVATION), new FormalField(String.class));
-            new Thread(new LockTimer(doorName)).start();
-            Object[] counter = ts.get(new ActualField(timerName), new FormalField(int.class));
-            ts.put(timerName, (int)counter[1]+1);
-            Thread.sleep(30000);
-            Object[] AuthorizedCrosser = ts.getp(new ActualField(doorName), new ActualField(AuthorizationManager.AUTHORIZED_CROSSER), new FormalField(String.class), new ActualField(activation[2]));
-
-            if (AuthorizedCrosser != null){
-                counter = ts.get(new ActualField(timerName), new FormalField(int.class));
-                int updatedCounter = (int)counter[1] - 1;
-                ts.put(timerName, updatedCounter);
-                if (updatedCounter > 0){
-                    ts.put(doorName, Door.LOCKING, true);
-                }
-            }
-
-
+    private void setInitialState() {
+        try { // Au départ, le compteur du timer est à 0
+            ts.put(timerID, 0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private void monitor() {
+        try {
+            monitorTimer();
+        } catch (InterruptedException e) {
+            System.out.println(timerID + " : error while communicating with the tuple space");
+            e.printStackTrace();
+        }
+        monitor();
+    }
+
+    private void monitorTimer() throws InterruptedException {
+            Object[] activation = ts.get(
+                    new ActualField(doorID),
+                    new ActualField(ACTIVATION),
+                    new FormalField(String.class));
+            String swipeCardId = (String) activation[2];
+
+            // Appel récursif pour le prochain timer
+            new Thread(new LockTimer(this)).start();
+
+            Object[] timerCounter = ts.get(
+                    new ActualField(timerID),
+                    new FormalField(Integer.class));
+            Integer counter = (Integer) timerCounter[1];
+
+            // On incrémente le compteur de timers actif pour la porte
+            ts.put(timerID, counter+1);
+
+            Thread.sleep(30000);
+
+            Object[] AuthorizedCrosser = ts.getp(
+                    new ActualField(doorID),
+                    new ActualField(AuthorizationManager.AUTHORIZED_CROSSER),
+                    new FormalField(String.class),
+                    new ActualField(swipeCardId));
+
+            if (AuthorizedCrosser != null){
+                // S'il reste quelqu'un d'autorisé à passer, on vérifie qu'il y a encore des timers en cours
+                // Si non il faut reverrouiller par sécurité
+                timerCounter = ts.get(
+                        new ActualField(timerID),
+                        new FormalField(Integer.class));
+                counter = (Integer) timerCounter[1];
+
+                int updatedCounter = counter - 1;
+                ts.put(timerID, updatedCounter);
+                if (updatedCounter > 0){
+                    ts.put(doorID, Door.LOCKING, true);
+                }
+            }
     }
 }
